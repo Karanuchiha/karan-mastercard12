19bcf96c4cda5c4a58f86598b101f242219701f2
Karanuchiha <bhardwajk528@gmail.com>
2019-10-30 23:50:08 +0530

    hu

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ var io = require('socket.io')(server);
 io.on('connection', function(socket) {
   socket.on('message', function(msg) {
     //msg=msg+"hello";
-    var s1="hi";
+    var s1="";
+    for(let i=msg.size()-1;i>=0;i--){
+      s1=s1+msg[i];
+    }
     io.emit('message', s1);
   });
 });
